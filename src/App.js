ef8c8dc565d794db6959a894abe2f540d145abfb
alimmondal <italimbd@gmail.com>
2021-06-21 14:08:37 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     password: '',
     photo: '',
   })
+
   var provider = new firebase.auth.GoogleAuthProvider();
   const fbProvider = new firebase.auth.FacebookAuthProvider();
   const handleGoogleSignIn = () => {
@@ -45,22 +46,16 @@ function App() {
   .signInWithPopup(fbProvider)
   .then((result) => {
     var credential = result.credential;
-    // The signed-in user info.
     var user = result.user;
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
     var accessToken = credential.accessToken;
-    console.log(user)
+    console.log(user, accessToken)
   })
   .catch((error) => {
-    // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    // The email of the user's account used.
     var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
     var credential = error.credential;
-
-    // ...
+    console.log(errorCode, errorMessage, email, credential)
   });
   }
 
